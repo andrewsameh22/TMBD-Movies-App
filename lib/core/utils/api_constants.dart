6419cd371cc7d63f7c44d0882127307b3705b519
Andrew Sameh <andrewsameh22@hotmail.com>
2024-04-26 19:22:52 +0300

    api constants add image url function

--- lib/core/utils/api_constants.dart
+++ lib/core/utils/api_constants.dart
@@ -1,4 +1,4 @@
-class AppConstants {
+class ApiConstants {
   static const String baseurl = "https://api.themoviedb.org/3";
   static const String apiKey = "46e11de373f0f984146aef4aa74f327a";
 
@@ -7,5 +7,7 @@ class AppConstants {
 
   static String movieDetailsEndpoint({required int movieId}) =>
       "$baseurl/movie/$movieId?api_key=$apiKey";
+  static const String baseImageUrl = 'https://image.tmdb.org/t/p/w500';
 
+  static String imageUrl(String path) => '$baseImageUrl$path';
 }

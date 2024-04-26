97b39f1c5ca4f8ebe6eaa250f173f4734b8c386d
Andrew Sameh <andrewsameh22@hotmail.com>
2024-04-26 19:27:27 +0300

    assets data

---
+++ lib/core/assets_data/assets_data.dart
@@ -0,0 +1,14 @@
+String svgsPath = "assets/svg";
+String imagesPath = "assets/images";
+
+class AssetsData {
+//svg
+  static String headerTitle = "$svgsPath/tmdb_header.png";
+
+  // logo
+  static String appLogo = "$svgsPath/app_logo.png";
+
+  //images
+  static String header = "$imagesPath/header.jpeg";
+  static String header2 = "$imagesPath/header2.png";
+}

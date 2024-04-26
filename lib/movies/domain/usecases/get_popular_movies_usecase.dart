93fb6ba7c62f557457ae439fc0519318de827378
Andrew Sameh <andrewsameh22@hotmail.com>
2024-04-26 19:44:09 +0300

    base usecase handling

--- lib/movies/domain/usecases/get_popular_movies_usecase.dart
+++ lib/movies/domain/usecases/get_popular_movies_usecase.dart
@@ -1,15 +1,17 @@
 import 'package:dartz/dartz.dart';
+import 'package:tmdb_movies_app/core/base_usecase/base_usecase.dart';
 import 'package:tmdb_movies_app/core/failures/failure.dart';
 import 'package:tmdb_movies_app/movies/domain/repository/base_movies_repository.dart';
 
 import '../entities/movie_entity.dart';
 
-class GetPopularMoviesUseCase {
+class GetPopularMoviesUseCase extends BaseUseCase<List<Movie>, NoParameters> {
   final BaseMoviesRepository baseMoviesRepository;
 
   GetPopularMoviesUseCase({required this.baseMoviesRepository});
 
-  Future<Either<Failure, List<Movie>>> call() async {
+  @override
+  Future<Either<Failure, List<Movie>>> call(NoParameters parameters) async {
     return await baseMoviesRepository.getPopularMovies();
   }
 }

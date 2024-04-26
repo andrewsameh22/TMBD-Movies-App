f00a8b31fbb8d97c69cd5019e263df0d6fcfb8af
Andrew Sameh <andrewsameh22@hotmail.com>
2024-04-26 20:28:04 +0300

    bloc observer

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,14 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 import 'core/app_router/routes.dart';
 import 'core/app_strings/app_strings.dart';
+import 'core/bloc_helper/my_bloc_observer.dart';
 import 'core/services/service_locator.dart';
 
 void main() {
   ServiceLocator().init();
+  Bloc.observer = MyBlocObserver();
 
   runApp(const MoviesApp());
 }
